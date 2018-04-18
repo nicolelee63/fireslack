2551acaa2826954eee3da0138431d69a7eb0ec56
nicolelee63 <nicole.lee633@gmail.com>
2018-04-18 21:44:19 +0800

    update

--- app/users/users.service.js
+++ app/users/users.service.js
@@ -1,23 +1,8 @@
 angular.module('angularfireSlackApp')
   .factory('Users', function($firebaseArray, $firebaseObject){
     var usersRef = firebase.database().ref('users');
-    var users = $firebaseArray(usersRef);
-
-    var usersRef = firebase.database().ref('users');
     var connectedRef = firebase.database().ref('.info/connected');
-
-    setOnline: function(uid){
-      var connected = $firebaseObject(connectedRef);
-      var online = $firebaseArray(usersRef.child(uid+'/online'));
-
-      connected.$watch(function (){
-        if(connected.$value === true){
-          online.$add(true).then(function(connectedRef){
-            connectedRef.onDisconnect().remove();
-          });
-        }
-      });
-    }
+    var users = $firebaseArray(usersRef);
 
     var Users = {
       getProfile: function(uid){
@@ -28,6 +13,18 @@ angular.module('angularfireSlackApp')
       },
       getGravatar: function(uid){
         return '//www.gravatar.com/avatar/' + users.$getRecord(uid).emailHash;
+      },
+      setOnline: function(uid){
+        var connected = $firebaseObject(connectedRef);
+        var online = $firebaseArray(usersRef.child(uid+'/online'));
+
+        connected.$watch(function (){
+          if(connected.$value === true){
+            online.$add(true).then(function(connectedRef){
+              connectedRef.onDisconnect().remove();
+            });
+          }
+        });
       },
       all: users
     };
